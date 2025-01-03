b30780b855edbcdc090066f6d9b857d5235618ba
martijnklomp1978 <martijn.klomp@gmail.com>
2025-01-03 11:40:04 +0100

    Update register.js

--- register.js
+++ register.js
@@ -6,28 +6,29 @@ document.getElementById("registerForm").addEventListener("submit", async (event)
   const password = document.getElementById("registerPassword").value;
 
   try {
+    // Verzend het POST-verzoek naar de backend
     const response = await fetch(`${backendUrl}/register`, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ email, password }),
     });
 
+    // Controleer de response
     if (!response.ok) {
     throw new Error(`Fout: ${response.status} ${response.statusText}`);
     }
 
+    // Verwerk de data
     const data = await response.json();
     document.getElementById("registerMessage").innerText = data.message;
 
     // Na succesvolle registratie, terug naar inloggen
-    if (response.ok) {
-      setTimeout(() => {
-        window.location.href = "index.html";
-      }, 2000);
-    }
+    setTimeout(() => {
+      window.location.href = "index.html";
+    }, 2000);
   } catch (error) {
-  console.error("Fout bij registreren:", error);
-  document.getElementById("registerMessage").innerText =
-    "Er is een fout opgetreden bij het registreren.";
-  }
+    console.error("Fout bij registreren:", error);
+    document.getElementById("registerMessage").innerText =
+      "Er is een fout opgetreden bij het registreren.";
+  }  
 });
